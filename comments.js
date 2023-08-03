4fdf45d30e65b88bfa2e32bbbd9d4f9f2cb7d024
oxanamar <126172702+oxanamar@users.noreply.github.com>
2023-08-03 11:00:41 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server
+// Path: server.js
+// var http = require("http");
+// var server = http.createServer(function(req, res) {
+//     console.log("Request was made: " + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end("Hey ninjas");
+// });
+// server.listen(3000, "
+
+
+
